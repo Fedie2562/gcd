76b954f1151eaaf0d680b3401be5af467808b294
fedie2562 <fedie2562@7HGM6Y1.Student.UGDSB.ED>
2018-10-30 14:59:14 -0400

    done

--- src/reducefraction/ReduceFraction.java
+++ src/reducefraction/ReduceFraction.java
@@ -6,6 +6,8 @@
 
 package reducefraction;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author fedie2562
@@ -16,14 +18,17 @@ public class ReduceFraction {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //
-        
+        // Input
+        int numerator = Integer.parseInt(JOptionPane.showInputDialog("Enter the numerator"));
+        int denominator = Integer.parseInt(JOptionPane.showInputDialog("Enter the denominator"));
+        System.out.println("The fraction "+numerator+"/"+denominator+" can be reduced to "
+                +numerator/gcd(numerator,denominator)+"/"+denominator/gcd(numerator,denominator));
     }
-    public static int gcd(int n){
-        if(n == 1){
-            return 1;
+    public static int gcd(int a,int b){
+        if(b == 0){
+            return a;
         }else{
-            return gcd(n);
+            return gcd(b, a%b);
         }
     }
 }
